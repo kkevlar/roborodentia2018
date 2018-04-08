56f349a303f1be86c437ae4fb963dabc45de585b
Kevin Kellar <kellar.kevin@gmail.com>
2018-04-07 20:28:17 -0700

    did main drive

--- DCMotorTest/DCMotorTest.ino
+++ DCMotorTest/DCMotorTest.ino
@@ -9,20 +9,51 @@
 
 #include "switch.h"
 
+void panic()
+{
+	digitalWrite(LED_BUILTIN, HIGH);
+}
+
 void setup() 
 {   
     pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
     motor_setup();
     switch_setup();
+    delay(1000);
+    go_stop();
+    delay(1000);
+    go_north();
+    if(test_switch_north(10000) != SUCCESS)
+    	panic();
+    go_stop();
 }
 
 void loop()
 {
-    int emergency = 1;
+	go_east();
+    if(test_switch_east(10000) != SUCCESS)
+    	panic();
     go_stop();
-    delay(1000);
     go_south();
-    if(test_switch_south(10000) == SUCCESS)
-        go_stop();
-    delay(15000);
+    if(test_switch_south(10000) != SUCCESS)
+    	panic();
+    go_stop();
+    go_north_west();
+    if(test_switch_north(10000) != SUCCESS)
+    	panic();
+    go_stop();
+    go_west();
+    if(test_switch_west(10000) != SUCCESS)
+    	panic();
+    go_stop();
+    go_south();
+    if(test_switch_south(10000) != SUCCESS)
+    	panic();
+    go_stop();
+    go_north_east();
+    if(test_switch_north(10000) != SUCCESS)
+    	panic();
+    stop();
+    delay(2000);
 }
